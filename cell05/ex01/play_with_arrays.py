d5d95149aed1daeaf7a5192e69bc343f9ba1c47d
dtwoeeanp <bhatia2550@gmail.com>
2025-06-22 08:53:57 +0000

    play_with_arrays

---
+++ cell05/ex01/play_with_arrays.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+my_list = [2, 8, 9, 48, 8, 22, -12, 2]
+my_list_plus_2 = []
+
+for num in my_list:
+    my_list_plus_2.append(num + 2)
+
+print("Original array:", my_list)
+print("New array:", my_list_plus_2)
