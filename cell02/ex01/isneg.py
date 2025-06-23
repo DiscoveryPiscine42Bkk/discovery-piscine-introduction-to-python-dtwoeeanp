a75f7817c3a09521e6a979086fb31206f21e54e2
dtwoeeanp <bhatia2550@gmail.com>
2025-06-23 03:10:28 +0000

    "edits"

--- cell02/ex01/isneg.py
+++ cell02/ex01/isneg.py
@@ -1,7 +1,7 @@
-number = int(input("Enter a number."))
-if number <0:
+no = int(input("Enter a number."))
+if no <0:
    print("The number is negative.")
-if number >0:
+if no >0:
    print("The number is positive.")
-if number ==0:
+if no ==0:
     print("The number is both positive and negative.")
--- cell04/ex04/round_up.py
+++ cell04/ex04/round_up.py
@@ -1,5 +1,5 @@
 import math
-user_input_str = input("Give me a number: ")
-user_input_float = float(user_input_str)
-rounded_up_float = math.ceil (user_input_float)
+input_str = input("Give me a number: ")
+input_float = float(input_str)
+rounded_up_float = math.ceil (input_float)
 print(rounded_up_float)
